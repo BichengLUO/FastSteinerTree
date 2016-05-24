341d42398c699297879d45866fad3a263a04494f
BichengLUO <nobodycrackme@gmail.com>
2016-05-25 00:28:38 +0800

    增加了一个main函数来进行算法的测试

--- src/com/faststeinertree/FastSteinerTree.java
+++ src/com/faststeinertree/FastSteinerTree.java
@@ -18,6 +18,10 @@ public class FastSteinerTree {
         return graph;
     }
 
+    public static void addEdge(int[][] graph, Edge e, int dist) {
+        graph[e.getNode1()][e.getNode2()] = graph[e.getNode2()][e.getNode1()] = dist;
+    }
+
     public FastSteinerTree(int[][] iGraph, int[] iSteinerPoints) {
         inputGraph = iGraph.clone();
         inputSteinerPoints = iSteinerPoints.clone();
@@ -43,6 +47,25 @@ public class FastSteinerTree {
 
     public void removeNonsteinerNodes() {
 
+    }
+
+    public static void main(String[] args) {
+        int[][] graph = new int[9][9];
+        addEdge(graph, new Edge(0, 1), 20);
+        addEdge(graph, new Edge(0, 8), 2);
+        addEdge(graph, new Edge(7, 8), 1);
+        addEdge(graph, new Edge(6, 7), 1);
+        addEdge(graph, new Edge(5, 6), 2);
+        addEdge(graph, new Edge(4, 5), 2);
+        addEdge(graph, new Edge(4, 9), 2);
+        addEdge(graph, new Edge(1, 5), 2);
+        addEdge(graph, new Edge(1, 2), 16);
+        addEdge(graph, new Edge(2, 4), 4);
+        addEdge(graph, new Edge(2, 3), 18);
+        addEdge(graph, new Edge(3, 4), 4);
+
+        FastSteinerTree fst = new FastSteinerTree(graph, new int[]{0 ,1, 2, 3});
+        int[][] st = fst.execute();
     }
 
 }
